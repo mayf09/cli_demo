2628a8687c58042f5f247f11ef4dc977c50550b4
mayf09 <mayfyx@163.com>
2020-08-24 17:20:02 +0800

    添加 cli2

---
+++ cli2.py
@@ -0,0 +1,31 @@
+import builtins
+
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--count', type=int, help='次数')
+@click.argument('name')
+def hello(name, count=1):
+    for x in range(count):
+        print('Hello %s!' % name)
+
+
+@cli.command()
+@click.argument('func', nargs=1)
+@click.argument('args', type=int, nargs=-1)
+def cal(func, args):
+    if func in ['sum', 'max', 'min']:
+        f = getattr(builtins, func)
+        print('{} = {}'.format(func, f(args)))
+    else:
+        raise ValueError('func_name error')
+
+
+if __name__ == '__main__':
+    cli()
